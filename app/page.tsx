a1fc362aa90ecdcd9687440b35b7206409c6019e
celescript <celestefernandez00@gmail.com>
2023-07-05 12:25:12 -0300

    [ADD] CHECK POSITION FUNCION

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
-'use client'
+"use client"
 
 const rows = 30
 const cols = 10
 
-type Grid = number[][]
-
+type Grid = Cell[][]
+type Cell = number
 // type Piece = Position[]
+type Position = [number, number]
 interface Piece {
-
   blocks: Position[]
 }
 
-type Position = [number, number]
-
-const grid: Grid = Array.from({ length: rows }, () => Array.from({ length: cols }, () => 0))
+const grid: Grid = Array.from(
+  { length: rows },
+  () => Array.from({ length: cols }, () => 0)
+  // grid es un array que por dentro tiene 30 veces (cant de rows) un array de 10 (cant de cols) items de largo
+)
 
 // x, y
 const piece: Piece = {
@@ -21,36 +23,50 @@ const piece: Piece = {
     [-1, -1],
     [0, -1],
     [-1, 0],
-    [0, 0]
-  ]
+    [0, 0],
+  ],
+}
+
+const getCell = (grid: Grid, position: Position): Cell => {
+  return grid[position[1]][position[0]]
 }
 
 // Return true if the piece can be in the position
-const checkPosition = (grid: Grid, piece: Piece, position: Position): boolean => {
-
+const checkPosition = (
+  grid: Grid,
+  piece: Piece,
+  position: Position
+): boolean => {
   // A cada coordenada de bloque del array pieza
- const result = piece.blocks.every((p) => {
-  // Le calculo la posicion de cada bloque
-  const positionToCheck: Position = [position[0] + p[0], position[1] + p[1]]
-// TODO: Chequear que el bloque este adentro de la grid
-  // can be more efficient to return !grid[...]
-  const valueAtPosition = grid[positionToCheck[1]][positionToCheck[0]]
+  const result = piece.blocks.every((p) => {
+    // Le calculo la posicion de cada bloque
+    // ej bloque [-1, -1] || position [0, 0]
+    const positionToCheck: Position = [position[0] + p[0], position[1] + p[1]] // [[0 + -1], [0 + -1]]
+    // [-1, -1]
 
-  if(valueAtPosition === 0) {
-    return true
-  } else {
-    return false
-  }
- })
+    // Chequeo si el bloque es parte de la grid
+    if (
+      positionToCheck[0] < 0 ||
+      positionToCheck[0] >= cols ||
+      positionToCheck[1] < 0 ||
+      positionToCheck[1] >= rows
+    ) {
+      return false
+    }
 
- return result
+    const valueAtPosition = getCell(grid, position)
 
+    // can be more efficient to return !grid[...]
+    if (valueAtPosition === 0) {
+      return true
+    } else {
+      return false
+    }
+  })
+
+  return result
 }
 
 export default function Home() {
-  return (
-    <main>
-      Hola que hace
-    </main>
-  )
+  return <main>Hola que hace</main>
 }
